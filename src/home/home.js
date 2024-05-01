1802093821bbf771d7053b23ab7638af949c53a6
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-05-02 00:59:31 +0530

    change: modularize home component

---
+++ src/home/home.js
@@ -0,0 +1,33 @@
+import "./home.css";
+
+const homeComponent = (onMenuClick, onReserveClick) => {
+  const containerDiv = document.createElement("div");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "ODIN RESTAURANT";
+  containerDiv.appendChild(h1);
+
+  const hr = document.createElement("hr");
+  containerDiv.appendChild(hr);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "Tasty and Affordable!";
+  containerDiv.appendChild(h2);
+
+  const buttonContainer = document.createElement("div");
+  const menuButton = document.createElement("button");
+  menuButton.textContent = "MENU";
+  menuButton.addEventListener("click", onMenuClick);
+  buttonContainer.appendChild(menuButton);
+
+  const reserveButton = document.createElement("button");
+  reserveButton.textContent = "RESERVE";
+  reserveButton.addEventListener("click", onReserveClick);
+  buttonContainer.appendChild(reserveButton);
+
+  containerDiv.appendChild(buttonContainer);
+
+  return containerDiv;
+};
+
+export { homeComponent };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import { homeComponent } from "./home/home";
 import "./styles.css";
+
+const elements = {
+  content: document.querySelector("#content"),
+  homeButton: document.querySelector(".home"),
+  menuButton: document.querySelector(".menu"),
+  contactButton: document.querySelector(".contact"),
+};
+
+const loadHome = () => elements.content.appendChild(homeComponent(loadMenu, loadContact));
+const loadMenu = () => console.log("menu");
+const loadContact = () => console.log("contact");
+
+loadHome();
